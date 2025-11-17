b8450c740eea66943a67124c4270ff80a9a39872
Pavel Kuznetsov <Technic47@gmail.com>
2025-11-17 18:05:10 +0300

    Parameter service added.

--- src/main/java/ru/kuznetsov/shop/operation/OperationApplication.java
+++ src/main/java/ru/kuznetsov/shop/operation/OperationApplication.java
@@ -2,8 +2,11 @@ package ru.kuznetsov.shop.operation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import ru.kuznetsov.shop.parameter.config.ParameterConfig;
 
 @SpringBootApplication
+@Import(ParameterConfig.class)
 public class OperationApplication {
 
 	public static void main(String[] args) {
