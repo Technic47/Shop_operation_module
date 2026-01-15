d692054cd5ed87416b073174ae6ab5d5ed91a945
Pavel Kuznetsov <Technic47@gmail.com>
2026-01-15 18:25:48 +0300

    Eureka discovery connection added.

--- src/main/java/ru/kuznetsov/shop/operation/OperationApplication.java
+++ src/main/java/ru/kuznetsov/shop/operation/OperationApplication.java
@@ -2,10 +2,12 @@ package ru.kuznetsov.shop.operation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 import ru.kuznetsov.shop.parameter.config.ParameterConfig;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @Import(ParameterConfig.class)
 public class OperationApplication {
 
